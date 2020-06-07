6e4a3608c0986a49fb61692df1d0c358f703f5e8
jakoza <62640612+jakoza@users.noreply.github.com>
2020-06-07 23:09:39 +0700

    gtw gimana

--- tantangan_training/database/migrations/2020_06_07_155042_create_inventories_table.php
+++ tantangan_training/database/migrations/2020_06_07_155042_create_inventories_table.php
@@ -16,9 +16,7 @@ class CreateInventoriesTable extends Migration
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
             $table->string('item_name');
-            $table->number('item_qtt');
-            $table->number('item_price');
-            $table->string('item_condition');
+            $table->integer('item_qtt');
             $table->timestamps();
         });
     }
